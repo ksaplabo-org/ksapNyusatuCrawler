a29cc05d74cc1407e6cdb8d43ecb09055d3b1b8b
ksaplabo <s.takada.ksap.labo@gmail.com>
2022-09-24 13:24:38 +0900

    modify functions

--- notifyNyusatuFunc/lambda_function.py
+++ notifyNyusatuFunc/lambda_function.py
@@ -17,7 +17,7 @@ def get_datetime():
     t_delta = datetime.timedelta(hours=9)
     JST = datetime.timezone(t_delta, 'JST')
     now = datetime.datetime.now(JST)
-    d = now.strftime('%Y%m%d%')
+    d = now.strftime('%Y%m%d')
     return d
 
 
